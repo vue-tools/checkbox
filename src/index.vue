e36cdd01d2ae357cecc2e297bf96aa63f955a53d
Jerret321 <321jiangtao@gmail.com>
2016-12-19 19:02:34 +0800

    checkbox component

---
+++ src/index.vue
@@ -0,0 +1,20 @@
+<style src="./style.css"></style>
+<template src="./template.html"></template>
+<script>
+    export default {
+        props: {
+            value: Boolean,
+            disabled: Boolean
+        },
+        computed: {
+            checkable: {
+                get(){
+                    return this.value
+                },
+                set(value){
+                    this.$emit('input', value)
+                }
+            }
+        }
+    }
+</script>
