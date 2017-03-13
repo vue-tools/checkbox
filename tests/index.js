74b79d47840d2d530de86dde60f770c92dd654c5
Jerret321 <321jiangtao@gmail.com>
2017-03-13 11:31:36 +0800

    add ci and tests

---
+++ tests/index.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+import  Checkbox  from '../src'
+import { expect } from 'chai'
+
+
+describe('vt-checkbox', () => {
+    it('checkbox default', () => {
+        let vm = new Vue({
+            render(h) {
+                return h('Checkbox', {}, 'input checkbox')
+            },
+            mounted(){
+                console.log('x')
+            },
+            components: {
+                Checkbox
+            }
+        }).$mount()
+        console.log(vm.$el)
+        let input = vm.$el.querySelector('.ui-checkbox__input')
+        expect(vm.$el.classList.contains('ui-checkbox')).to.equal(true)
+        expect(!!input).to.equal(true)
+        expect(input.disabled).to.equal(false)
+        expect(input.checked).to.equal(false)
+        expect(vm.$el.querySelector('.ui-checkbox__label').innerHTML).to.equal('input checkbox')
+    })
+    it('checkbox disabled', () => {
+        let vm = new Vue({
+            render(h) {
+                return (
+                    <Checkbox disabled>input checkbox</Checkbox>
+                )
+            },
+            components: {
+                Checkbox
+            }
+        }).$mount()
+        expect(vm.$el.classList.contains('ui-checkbox')).to.equal(true)
+        expect(vm.$el.querySelector('.ui-checkbox__input').disabled).to.equal(true)
+    })
+    it('checkbox checked', () => {
+        let vm = new Vue({
+            render(h) {
+                return (
+                    <Checkbox v-model="v"></Checkbox>
+                )
+            },
+            data() {
+                return {
+                    v: true
+                }
+            },
+            components: {
+                Checkbox
+            }
+        }).$mount()
+
+        let input = vm.$el.querySelector('.ui-checkbox__input')
+        expect(vm.$el.classList.contains('ui-checkbox')).to.equal(true)
+        expect(input.checked).to.equal(false)
+    })
+})
---
+++ tests/karma.conf.js
@@ -0,0 +1,41 @@
+let webpackTestConfig = require('./webpack.test.conf')
+
+module.exports = function (config) {
+    config.set({
+        browsers: [/*'PhantomJS'*/, 'Chrome'],
+        frameworks: ['mocha', 'promise', 'sinon'],
+        files: ['./index.js'],
+        preprocessors: {
+            './index.js': ['webpack', 'sourcemap']
+        },
+        reporters: ['spec', 'coverage'],
+        coverageReporter: {
+            dir: './coverage',
+            reporters: [{
+                type: 'text'
+            }, {
+                type: 'lcov',
+                subdir: '.'
+            }, {
+                type: 'json',
+                subdir: '.'
+            }]
+        },
+        plugins: [
+            'karma-mocha',
+            'karma-sinon',
+            'karma-promise',
+            'karma-webpack',
+            'karma-coverage',
+            'karma-spec-reporter',
+            'karma-sourcemap-loader',
+            'karma-phantomjs-launcher',
+            'karma-chrome-launcher'
+        ],
+        webpack: webpackTestConfig,
+        webpackMiddleware: {
+            noInfo: true
+        },
+        singleRun: true
+    })
+}
